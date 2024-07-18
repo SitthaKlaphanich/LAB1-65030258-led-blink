b33a920ad4739aed35f8a0e8e1ec7cbe0ec2a705
SitthaKlaphanich <ao2604152014@hotmail.com>
2024-07-18 15:49:40 +0700

    LED_PUSH

--- main/main.c
+++ main/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
+#define BUTTON_PIN 4
+#define LED_PIN 5
 
 void app_main(void)
 {
-    int i = 0;
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+
     while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        if (gpio_get_level(BUTTON_PIN) == 0) {
+            gpio_set_level(LED_PIN, 1);  // ถ้ากดสวิตช์ LED ติด
+        } else {
+            gpio_set_level(LED_PIN, 0);  // ถ้าปล่อยสวิตช์ LED ดับ
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);  // รอสักครู่
     }
 }
